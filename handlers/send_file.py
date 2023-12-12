19c99c397efd1692e839a6f36d5bf3e1af253b7a
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-13 00:53:42 +0100

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -35,6 +35,7 @@ async def reply_forward(message: Message, file_id: int):
                 ]
             )
         )
+        
     except FloodWait as e:
         await asyncio.sleep(e.x)
         await reply_forward(message, file_id)
@@ -51,5 +52,5 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(sent_message, file_id)  # Removed "message=" from this line
+    await reply_forward(sent_message, file_id)
     asyncio.create_task(delete_after_delay(sent_message, 3600))

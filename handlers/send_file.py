05ef25d5c92d5f47279f96b08f51e45c2177c0cc
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-13 00:51:02 +0100

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -16,6 +16,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_markup(
+            disable_web_page_preview=True,
+            quote=True,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
@@ -33,9 +35,6 @@ async def reply_forward(message: Message, file_id: int):
                 ]
             )
         )
-        disable_web_page_preview=True,
-            quote=True,
-        
     except FloodWait as e:
         await asyncio.sleep(e.x)
         await reply_forward(message, file_id)
@@ -43,21 +42,14 @@ async def reply_forward(message: Message, file_id: int):
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id)
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
-            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_ids=file_id)
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_ids=file_id)
     except FloodWait as e:
-        await asyncio.sleep(e.value)
-        return media_forward(bot, user_id, file_id)
-        await message.delete()
+        await asyncio.sleep(e.x)
+        return await media_forward(bot, user_id, file_id)
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
-    await reply_forward(message=sent_message, file_id=file_id)
+    await reply_forward(sent_message, file_id)  # Removed "message=" from this line
     asyncio.create_task(delete_after_delay(sent_message, 3600))
-
-async def delete_after_delay(message, delay):
-    await asyncio.sleep(delay)
-    await message.delete()

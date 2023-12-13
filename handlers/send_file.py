9c62d91d6e0502ecbb1623643b926ec6f97e2e41
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-13 02:26:37 +0100

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -12,7 +12,7 @@ from handlers.helpers import str_to_b64
 
 async def reply_forward(message: Message, file_id: int):
     try:
-        await message.reply_text(
+        await message.reply_text()
             f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
             disable_web_page_preview=True,
          quote=True

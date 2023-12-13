ab828dbb2c77833369f40e69de800d52de078470
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-13 02:41:49 +0100

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -19,7 +19,7 @@ async def reply_forward(message: Message, file_id: int):
             f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
             disable_web_page_preview=True,
             quote=True,
-            reply_forward=InlineKeyboardMarkup(
+            reply_markup=InlineKeyboardMarkup(
                 [
                     [
                         InlineKeyboardButton("𝖬𝖺𝗂𝗇 𝖢𝗁𝖺𝗇𝗇𝖾𝗅", url="https://t.me/silvervoidmovie")

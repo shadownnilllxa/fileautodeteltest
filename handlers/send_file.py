ec38e55be0a099d42284c82ea5dbeb5d6c5d207a
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-13 02:47:26 +0100

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -17,7 +17,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceRepl
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
+            f"Files will be deleted in 1 hour to avoid copyright issues. Please forward and save them.",
             disable_web_page_preview=True,
             quote=True,
             reply_markup=InlineKeyboardMarkup(
@@ -57,7 +57,7 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
     await reply_forward(message=sent_message, file_id=file_id)
-    asyncio.create_task(delete_after_delay(sent_message, 1800))
+    asyncio.create_task(delete_after_delay(sent_message, 3600))
 
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)

7581d9ac4e75d605874e97650106a3a4fd21b8aa
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-23 21:31:59 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -46,9 +46,9 @@ class Config(object):
 [Donate Me](https://t.me/Snowball_Official)
 """
 	HOME_TEXT = """
-Hello, [{}](tg://user?id={})\n\n☁️ Welcome to the Telegram Cloud Space! 
-This Bot can save all your documents! 
-Send me any photo, video, GIF, file, etc. to save it.
+Hello, [{}](tg://user?id={})\n\n**☁️ Welcome to the Telegram Cloud Space!** 
+*This Bot can save all your documents! 
+Send me any photo, video, GIF, file, etc. to save it.*
 
 """
 	

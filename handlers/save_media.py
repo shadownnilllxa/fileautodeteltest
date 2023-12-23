b836e54cd520189d8034244e27f5a0d87a5312c6
Salxchange <138751618+Salxchange@users.noreply.github.com>
2023-12-23 17:52:38 +0100

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -50,6 +50,11 @@ async def save_batch_media_in_channel(bot: Client, editable: Message, message_id
             ]])
         )
         share_link =f"https://t.me/{Config.BOT_USERNAME}?start=PredatorHackerzZ_{str_to_b64(str(SaveMessage.id))}"
+        await editable.edit("**𝖲𝗍𝖺𝗋𝗍𝗂𝗇𝗀 𝖡𝗈𝗍**")
+    await asyncio.sleep(2)
+    await editable.edit("**♻️ 𝖫𝗈𝖺𝖽𝗂𝗇𝗀...**")
+    await asyncio.sleep(2)    
+    await editable.delete()
         await editable.edit(
             f"**Batch Files Stored in my Database!**\n\nHere is the Permanent Link of your files: <a href={share_link}>Link</a>\n\n"
             f"Just Click the link to get your files!",

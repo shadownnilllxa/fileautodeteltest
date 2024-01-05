8300c922c9a915746ecd52a0a886b1bf60b5d1bc
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-01-05 02:46:30 +0100

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,7 +12,7 @@ class Config(object):
 	SHORTLINK_API = os.environ.get('SHORTLINK_API')
 	BOT_OWNER = int("5774951539")
 	DATABASE_URL=("mongodb+srv://ROKU:ROKU@cluster0.nxjre0s.mongodb.net/?retryWrites=true&w=majority")
-	UPDATES_CHANNEL=("-1001816446904")
+	UPDATES_CHANNEL=("-1002107653098")
 	LOG_CHANNEL=("-1001941319109")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
